f84a84dd8e412debcf8f799482351bf7be384992
Muhie <85196209+Muhie@users.noreply.github.com>
2022-02-04 13:18:16 +0000

    Camera yeah

--- robot.py
+++ robot.py
@@ -1,19 +1,20 @@
 from sr.robot3 import *
 print("hello world")
-import time 
+import time
 
 
 
 class Movement():
     def __init__(self, R, motorboard1, motorboard2):
         self.R = R
+        self.marker_ids = R.camera.save(R.usbkey / "initial-view.png")
         self.fb = motorboard1
         self.bb = motorboard2
         self.fb_l_power = 0
         self.fb_r_power = 0
         self.bb_l_power = 0
         self.bb_r_power = 0
-        self.master_power = 0.25
+        self.master_power = 0.15
     
     def move(self):
         self.R.motor_boards[self.fb].motors[0].power = self.fb_l_power
@@ -50,15 +51,15 @@ class Movement():
         self.move()
 
     def start(self):
-        while True:
-            self.forwards()
-            time.sleep(5)
-            self.backwards()
-            time.sleep(5)
-            self.left()
-            time.sleep(5)
-            self.right()
-            time.sleep(5)
+        
+        self.forwards()
+        time.sleep(5)
+        self.backwards()
+        time.sleep(5)
+        self.left()
+        time.sleep(5)
+        self.right()
+        time.sleep(5)
 
 
 
@@ -68,8 +69,18 @@ def main():
     motorboard2 = 'SR0JH18' #Back motorboard
     robo = Movement(R, motorboard1, motorboard2)
     robo.start()
+    marker_ids = R.camera.save(R.usbkey / "initial-view.png")
+
+    while True:
+        markers = R.camera.see()
+        print("I can see", len(markers), "markers:")
+
+        for m in markers:
+            print(" - Marker #{0} is {1} metres away".format(m.id, m.distance))
+
 
 
 
 if __name__ == '__main__':
     main()
+
