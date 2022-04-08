c80458604f198e05b0c5b68d51c9f1382b6d2824
Muhie <85196209+Muhie@users.noreply.github.com>
2022-04-08 13:28:34 +0100

    Can detection now works

--- robot.py
+++ robot.py
@@ -1,62 +1,80 @@
 from sr.robot3 import *
 import cv2
 import numpy as np
+import time
 class Collybot(Robot):
     def __init__(self):
         super().__init__()
         self.classNames = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'street sign', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'hat', 'backpack', 'umbrella', 'shoe', 'eye glasses', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'plate', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'mirror', 'dining table', 'window', 'desk', 'toilet', 'door', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'blender', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush', 'hair brush']
         path = str(self.usbkey)
+        self.path1 = path
         self.weightsPath = path+"/frozen_inference_graph.pb"
-        self.configPath ="ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+        self.configPath =path+"/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
         print(self.weightsPath,self.configPath)
         self.net = cv2.dnn_DetectionModel(self.weightsPath,self.configPath)
         self.net.setInputSize(200,200)
         self.net.setInputScale(1.0/ 127.5)
         self.net.setInputMean((127.5,127.5,127.5))
         self.net.setInputSwapRB(True)
+        self.marker_ids = self.camera.save(self.usbkey / "initial-view.png")
+    def start(self):
+        for i in range(1,20):
+            print("pass")
+            print(i)
+            time.sleep(0.2)
+            try:
+                self.camera.save(self.usbkey / "can_Detection.png")
+                time.sleep(0.3)
+                print("trying to find cans")
+                self.can_Regonition()
+            except:
+                self.camera.save(self.usbkey / "can_Detection.png")
+                print("taking picture")
+                self.can_Regonition()
     def can_Regonition(self):
-        img = cv2.imread(R.camera.see())
-        while True:
-            #success,img = self.cap.read()
-            classIds,confs,bbox = self.net.detect(img,confThreshold=0.4)
-            #if len(classIds) >= 1:
-            for classId,confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
-                can = self.classNames[classId-1]
-                self.confs1 = confs
-                self.img1 = img
-                self.box1 = box
-                boxy = np.array_str(box)
-                coords = boxy.replace(' ', ',')
-                formatted = coords.replace('[','')
-                fullyformatted = formatted.replace(']','')
-                self.boxposition = box
-                #if can == "bottle":
-                    #print("I found something useful")
-                    #print(can)
-                    #print(fullyformatted)
-                    #cv2.rectangle(img,box,color=(0,255,0),thickness = 2)
-                    #self.where_CanX()
-                    #self.where_CanY()
-                if can == "bowl":
-                    print("I found a can")
-                    #print(fullyformatted)
-                    self.where_CanX()
-                    self.where_CanY()
-                elif can == "cup":
-                    print("I found a can")
-                    print(fullyformatted)
-                    self.where_CanX()
-                    self.where_CanY()
-                elif can == "cell phone":
-                    print("I found something useful")
-                    print(can)
-                    print(fullyformatted)
-                    self.where_CanX()
-                    self.where_CanY()
-                else:
-                    pass
-            cv2.imshow("Can Detector",img)
-            cv2.waitKey(0)
+        print("opening file!")
+        img = cv2.imread(self.path1+"/can_Detection.png")
+        #success,img = self.cap.read()
+        classIds,confs,bbox = self.net.detect(img,confThreshold=0.2)
+        #if len(classIds) >= 1:
+        for classId,confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+            can = self.classNames[classId-1]
+            self.confs1 = confs
+            self.img1 = img
+            self.box1 = box
+            boxy = np.array_str(box)
+            coords = boxy.replace(' ', ',')
+            formatted = coords.replace('[','')
+            fullyformatted = formatted.replace(']','')
+            self.boxposition = box
+            #if can == "bottle":
+                #print("I found something useful")
+                #print(can)
+                #print(fullyformatted)
+                #cv2.rectangle(img,box,color=(0,255,0),thickness = 2)
+                #self.where_CanX()
+                #self.where_CanY()
+            if can == "bowl":
+                print("I found a can")
+                #print(fullyformatted)
+                self.where_CanX()
+                self.where_CanY()
+                self.power_board.piezo.buzz(0.1, Note.D6)
+            elif can == "cup":
+                print("I found a can")
+                print(fullyformatted)
+                self.where_CanX()
+                self.where_CanY()
+                self.power_board.piezo.buzz(0.1, Note.D6)
+            #elif can == "cell phone":
+                #print("I found something useful")
+                #print(can)
+                #print(fullyformatted)
+                #self.where_CanX()
+                #self.where_CanY()
+            else:
+                print("can not found")
+                pass
     def where_CanX(self):
         print(self.boxposition)
         if self.boxposition[0] >= 450:
@@ -85,6 +103,6 @@ class Collybot(Robot):
 
 def main():
     jeff = Collybot()
-    jeff.can_Regonition()
+    jeff.start()
 if __name__ == '__main__':
     main()
